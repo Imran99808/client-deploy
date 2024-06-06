40ae89cb1a670b41883b934b3d1f3bd14bd04c69
Èmràñ Rèzà <103001711+Imran99808@users.noreply.github.com>
2024-06-06 08:02:13 -0700

    Update Footer.js
    footer update cart values

--- app/Components/Footer.js
+++ app/Components/Footer.js
@@ -1,12 +1,15 @@
 'use client'
 import React from 'react'
 // import {Dink} from '@/app/lib/Link'
+import React, { useContext } from 'react'
+import { theme } from '../context/AddContext';
 import Link from 'next/link'
 import { IonIcon } from '@ionic/react'
 
 //  import {IonIcon} from '@ionic/react'
 import { logoIonic, heartOutline, homeOutline, gridOutline, bagOutline, settingsOutline } from 'ionicons/icons'
 export default function Footer() {
+  const {values}=useContext(theme); 
   const c = '/categories'
   return (
     <div className=' nav bg-[#fff] fixed bottom-0 right-0 left-0'>
@@ -20,7 +23,7 @@ export default function Footer() {
             <IonIcon icon={gridOutline} /></Link></li>
           <li className='text-3xl relative   '>
           
-            <span className=' absolute top-[-5px] left-5    bg-red-600 text-xs  pl-1 pe-1 rounded-full text-[#fff]'>10</span>
+            <span className={` absolute top-[-5px] left-5 ${values.length===0&&'hidden'}   bg-red-600 text-xs  pl-1 pe-1 rounded-full text-[#fff]`}>{values.length}</span>
             <Link
               href={{
                 pathname: "/cart",
